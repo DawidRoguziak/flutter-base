e1141bf41bdc6d71e4f49f734f5fea0a4700b8f6
dawidroguziak@gmail.com <MNB!@#321zx>
2022-11-01 16:55:35 +0100

    clear files

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_finance_calculator/pages/PageLogin.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-
   runApp(const ProviderScope(child: RootWidget()));
 }
 
@@ -37,33 +29,5 @@ class RootWidget extends StatelessWidget {
                 backgroundColor: Color(0xAAee4266))),
         themeMode: ThemeMode.light,
         home: PageLogin());
-
-    // return MaterialApp(
-    //     title: 'MFC',
-    //     theme: ThemeData(
-    //         fontFamily: 'Raleway',
-    //         brightness: Brightness.dark,
-    //         primaryColor: const Color(0xAAee4266),
-    //         floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    //             foregroundColor: Colors.white,
-    //             backgroundColor: Color(0xAAee4266))),
-    //     darkTheme: ThemeData(
-    //         fontFamily: 'Raleway',
-    //         brightness: Brightness.dark,
-    //         primaryColor: const Color(0xAAee4266),
-    //         floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    //             foregroundColor: Colors.white,
-    //             backgroundColor: Color(0xAAee4266))),
-    //     themeMode: ThemeMode.light,
-    //     home: StreamBuilder<User?>(
-    //       stream: FirebaseAuth.instance.authStateChanges(),
-    //       builder: (context, snapshot) {
-    //         if (snapshot.hasData) {
-    //           return RandomSelectorPage();
-    //         } else {
-    //           return const PageLogin();
-    //         }
-    //       },
-    //     ));
   }
 }
